38549db273093269e53f9129c0d8570b2895a66c
fong <isfong@qq.com>
2019-05-16 18:24:14 +0800

    add mybatis plus config class

--- spring-boot/spring-boot-apply/src/main/java/com/isfong/spring/boot/apply/mapper/UserMapper.java
+++ spring-boot/spring-boot-apply/src/main/java/com/isfong/spring/boot/apply/mapper/UserMapper.java
@@ -2,8 +2,6 @@ package com.isfong.spring.boot.apply.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.isfong.spring.boot.apply.model.User;
-import org.apache.ibatis.annotations.Mapper;
 
-@Mapper
 public interface UserMapper extends BaseMapper< User > {
 }
